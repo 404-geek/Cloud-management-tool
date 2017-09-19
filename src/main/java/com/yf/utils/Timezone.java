5cc8978730740f40aa01c1582ffba95124c5347c
pranayaptus <pranay.das@aptusdatalabs.com>
2017-09-19 17:15:36 +0530

    up

--- src/main/java/com/yf/utils/Timezone.java
+++ src/main/java/com/yf/utils/Timezone.java
@@ -1,10 +1,14 @@
 package com.yf.utils;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 public class Timezone {
 	static Logger LOGGER = Logger.getLogger(Timezone.class.getName());
-	
+	public static ArrayList<String> getvm(){
+		
+		
+	}
 	
 	
 }
