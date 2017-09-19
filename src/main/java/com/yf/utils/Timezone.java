082da8795d2729b00fc0194334dd8c81adeaace2
pranayaptus <30833462+pranayaptus@users.noreply.github.com>
2017-09-19 17:24:09 +0530

    Update Timezone.java

--- src/main/java/com/yf/utils/Timezone.java
+++ src/main/java/com/yf/utils/Timezone.java
@@ -5,8 +5,13 @@ import java.util.logging.Logger;
 
 public class Timezone {
 	static Logger LOGGER = Logger.getLogger(Timezone.class.getName());
-	public static ArrayList<String> getvm(){
-		
+	public static ArrayList<String> getTimeZone(){
+		String[] ids = TimeZone.getAvailableIDs();
+		 ArrayList<String> list=new ArrayList<String>();
+		for (String id : ids) {
+			list.add(id);
+		}
+		return list;
 		
 	}
 	
