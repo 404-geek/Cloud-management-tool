2ecfe60436dbbe4e4426c23fd3c0cd7631022fc9
pranayaptus <pranay.das@aptusdatalabs.com>
2017-09-22 20:59:38 +0530

    update

--- src/main/java/com/yf/utils/AzureAuth.java
+++ src/main/java/com/yf/utils/AzureAuth.java
@@ -20,38 +20,30 @@ public class AzureAuth {
 
 		RequestBody formBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
 				.addFormDataPart("client_id", "b3144fe1-6338-43a0-8918-c05291f51170")
-				.addFormDataPart("grant_type", "authorization_code")
-				.addFormDataPart("code", token)
+				.addFormDataPart("grant_type", "authorization_code").addFormDataPart("code", token)
 				.addFormDataPart("redirect_uri", "https://tpconnect.yellowfin.bi/getToken.jsp")
 				.addFormDataPart("client_secret", "L7mq5+HjRZo3NCnJ1vjR4E0FWubFkiC+KHe2+hd2xd0=")
-				.addFormDataPart("resource", "https://management.core.windows.net/")
-				.build();
+				.addFormDataPart("resource", "https://management.core.windows.net/").build();
 
-		Request request = new Request.Builder()
-				.url("https://login.microsoftonline.com/common/OAuth2/token")
-				.post(formBody)
-				.build();
+		Request request = new Request.Builder().url("https://login.microsoftonline.com/common/OAuth2/token")
+				.post(formBody).build();
 
 		try {
 			Response response = client.newCall(request).execute();
 			return response.code();
 		} catch (Exception e) {
-
 			return responseCode;
 		}
 
 	}
 
 	public static String getResponse(String token) {
-  
-	
 
 		OkHttpClient client = new OkHttpClient();
 
 		RequestBody formBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
 				.addFormDataPart("client_id", "b3144fe1-6338-43a0-8918-c05291f51170")
-				.addFormDataPart("grant_type", "authorization_code")
-				.addFormDataPart("code", token)
+				.addFormDataPart("grant_type", "authorization_code").addFormDataPart("code", token)
 				.addFormDataPart("redirect_uri", "https://tpconnect.yellowfin.bi/getToken.jsp")
 				.addFormDataPart("client_secret", "L7mq5+HjRZo3NCnJ1vjR4E0FWubFkiC+KHe2+hd2xd0=")
 				.addFormDataPart("resource", "https://management.core.windows.net/").build();
@@ -63,7 +55,7 @@ public class AzureAuth {
 			return response.body().string();
 		} catch (Exception e) {
 
-			return "";
+			return null;
 		}
 
 	}
