9a7fb7c315b9c8abccdf77fcb3e75aae9f6fc0b1
pranayaptus <pranay.das@aptusdatalabs.com>
2017-09-26 18:38:17 +0530

    update

--- src/main/java/com/yf/utils/Billing.java
+++ src/main/java/com/yf/utils/Billing.java
@@ -24,7 +24,7 @@ public class Billing {
 
 		Request request = new Request.Builder()
 				.url("https://management.azure.com" + id
-						+ "/providers/Microsoft.Commerce/RateCard?api-version=2016-08-31-preview&$filter=OfferDurableId eq 'MS-AZR-0003p' and Currency eq 'USD' and Locale eq 'en-US' and RegionInfo eq 'US'")
+						+ "/providers/Microsoft.Commerce/RateCard?api-version=2016-08-31-preview&$filter=OfferDurableId eq 'MS-AZR-0003p' and Currency eq 'AUD' and Locale eq 'en-AU' and RegionInfo eq 'AU'")
 				.addHeader("Authorization", tok).addHeader("Content-type", CONTENT).build();
 		try {
 			Response response = client.newCall(request).execute();
@@ -32,29 +32,8 @@ public class Billing {
 			JsonObject jo = je.getAsJsonObject();
 			JsonArray ja1 = jo.getAsJsonArray("Meters");
 			String currency = jo.get("Currency").getAsString();
-			String tax = jo.get("IsTaxIncluded").getAsString();
-			String locale = jo.get("Locale").getAsString();
 			for (int j = 0; j < ja1.size(); j++) {
 				JsonObject jo1 = new JsonObject();
-/*				jo1.addProperty("Currency", currency);
-				jo1.addProperty("Tax", tax);
-				jo1.addProperty("Locale", locale);
-				jo1.addProperty("EffectiveDate", ja1.get(j).getAsJsonObject().get("EffectiveDate").getAsString());
-				jo1.addProperty("IncludedQuantity", ja1.get(j).getAsJsonObject().get("IncludedQuantity").getAsInt());
-				try {
-					jo1.addProperty("MeterCategory", ja1.get(j).getAsJsonObject().get("MeterCategory").getAsString());
-				} catch (Exception e) {
-					jo1.addProperty("MeterCategory", "NULL");
-				}
-				jo1.addProperty("MeterRegion", ja1.get(j).getAsJsonObject().get("MeterRegion").getAsString());
-				jo1.addProperty("MeterStatus", ja1.get(j).getAsJsonObject().get("MeterStatus").getAsString());
-				jo1.addProperty("MeterSubCategory", ja1.get(j).getAsJsonObject().get("MeterSubCategory").getAsString());
-				try {
-					jo1.addProperty("MeterTags", ja1.get(j).getAsJsonObject().get("MeterTags").getAsString());
-				} catch (Exception e) {
-					jo1.addProperty("MeterTags", "NULL");
-				}
-				jo1.addProperty("Unit", ja1.get(j).getAsJsonObject().get("Unit").getAsString());*/
 				jo1.addProperty("MeterId", ja1.get(j).getAsJsonObject().get("MeterId").getAsString());
 				try {
 					jo1.addProperty("MeterRates",
@@ -71,7 +50,7 @@ public class Billing {
 		return ja.toString();
 	}
 
-	public static String getBilling(String token) {
+	public static String getBillingLive(String token) {
 		BigDecimal rate = new BigDecimal(123);
 		String CONTENT = "application/json";
 		JsonArray ja = new JsonArray();
@@ -115,5 +94,53 @@ public class Billing {
 			return null;
 		}
 		return ja.toString();
+	}
+	
+		public static String getBilling(String token) {
+			BigDecimal rate = new BigDecimal(123);
+			String CONTENT = "application/json";
+			JsonArray ja = new JsonArray();
+			String id = Subscriptions.getId(token);
+			String tok = "Bearer " + token;
+			OkHttpClient client = new OkHttpClient();
+			LocalDate localDate = LocalDate.now();
+			LocalDate ago = LocalDate.now().minusDays(1);
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+			Request request = new Request.Builder()
+					.url("https://management.azure.com" + id
+							+ "/providers/Microsoft.Commerce/UsageAggregates?api-version=2015-06-01-preview&reportedStartTime="
+							+ ago.format(formatter) + "&reportedEndTime=" + localDate.format(formatter)
+							+ "&aggregationGranularity=Hourly&showDetails=True")
+					.addHeader("Authorization", tok).addHeader("Content-type", CONTENT).build();
+			try {
+				Response response = client.newCall(request).execute();
+         		JsonElement je = new JsonParser().parse(response.body().string());
+				JsonObject jo = je.getAsJsonObject();
+				JsonArray ja1 = jo.getAsJsonArray("value");
+				for (int j = 0; j < ja1.size(); j++) {
+					JsonObject jo1 = new JsonObject();
+					String resp = getCard(token);
+					JsonElement match = new JsonParser().parse(resp);
+					JsonArray ja2 = match.getAsJsonArray();
+					String i = ja1.get(j).getAsJsonObject().get("meterId").getAsString();
+					for(int k=0;k<ja2.size();k++){
+						String temp = ja2.get(k).getAsJsonObject().get("MeterId").getAsString();
+						if(i.equals(temp)){
+							rate = ja2.get(k).getAsJsonObject().get("MeterRates").getAsBigDecimal();
+						}
+					}
+					jo1.addProperty("id", ja1.get(j).getAsJsonObject().get("id").getAsString());
+					jo1.addProperty("SubscriptionId", ja1.get(j).getAsJsonObject().get("properties").getAsJsonObject().get("subscriptionId").getAsString());
+					BigDecimal quantity = ja1.get(j).getAsJsonObject().get("properties").getAsJsonObject().get("quantity").getAsBigDecimal();
+					jo1.addProperty("BillAmount(USD)",rate.multiply(quantity));
+					ja.add(jo1);
+				}
+
+			} catch (Exception e) {
+				return null;
+			}
+			return ja.toString();
+		
+		
 	}
 }
--- src/main/java/com/yf/utils/Test2.java
+++ src/main/java/com/yf/utils/Test2.java
@@ -14,8 +14,8 @@ import okhttp3.Response;
 
 public class Test2 {
 	public static void main(String[] args) {
-		
-		String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkhIQnlLVS0wRHFBcU1aaDZaRlBkMlZXYU90ZyIsImtpZCI6IkhIQnlLVS0wRHFBcU1aaDZaRlBkMlZXYU90ZyJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuY29yZS53aW5kb3dzLm5ldC8iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8yN2M5NTA0YS1jNDY3LTQzNzYtYjI2MS04OWIxNWU4ZWJiMTcvIiwiaWF0IjoxNTA2MzQzNDUxLCJuYmYiOjE1MDYzNDM0NTEsImV4cCI6MTUwNjM0NzM1MSwiYWNyIjoiMSIsImFpbyI6IkFTUUEyLzhGQUFBQUt0a0lTbXU5QXlmV2U5SzhTaTJpY0ZUUjNzY1Z4U0taZGZyUm1KZ0oxcWs9IiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6ImIzMTQ0ZmUxLTYzMzgtNDNhMC04OTE4LWMwNTI5MWY1MTE3MCIsImFwcGlkYWNyIjoiMSIsImZhbWlseV9uYW1lIjoiSGV3aXR0IiwiZ2l2ZW5fbmFtZSI6Ikp1c3RpbiIsImdyb3VwcyI6WyJhYzc5Njc0MC1mZjVjLTQ3ZjctODlkZS1mZTkzM2JkZjUwOGQiXSwiaXBhZGRyIjoiMTQuMTQyLjExMC4yMjIiLCJuYW1lIjoiSnVzdGluIEhld2l0dCIsIm9pZCI6IjVmODkwNmQxLWVlMmQtNDIxZC1hYTcxLTA2YTgxMDljMjEzMCIsInB1aWQiOiIxMDAzM0ZGRjhEMjcyNEFDIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoicHJZRGlfbC1HR2xPempfaUw2T2t6cEFLdEFvQ2RUZWxHMFZ1UnM5TlJtWSIsInRpZCI6IjI3Yzk1MDRhLWM0NjctNDM3Ni1iMjYxLTg5YjE1ZThlYmIxNyIsInVuaXF1ZV9uYW1lIjoianVzdGluLmhld2l0dEB5ZWxsb3dmaW4uYmkiLCJ1cG4iOiJqdXN0aW4uaGV3aXR0QHllbGxvd2Zpbi5iaSIsInV0aSI6ImNnYUpzbzhaRVVHdDUtenN6Rm9VQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjYyZTkwMzk0LTY5ZjUtNDIzNy05MTkwLTAxMjE3NzE0NWUxMCJdfQ.AQfDbkRNN56peIR7lcumYoFmVVst5fhNOuW1oqy050K267nWvM_6xYAdFRDYp87-JdFAGqFCJ4fa_It76LQSjbGSpi2XV2FAdtOamV1gHEY5_oZ52lFg0eCUoShaOrXQztYxEybjEoUsJQpZgRgiiAH38h3Hp3kk3-gLu78-fpHcvuMVXhMhKjb7BxZWqUll6XsXLBWWGTIjhfMzzp_xXdz2Vy9Cw4_emcVNWQVUi2GzK9kKeemlf__VHk2Qze72yS-AnNzL3xyCmz79AtufKkvIV81dGvxSxX9EcfsOr8aUu8iYWnQ3PN8a0968z0Q1IC7eGhFM1dflrVL_cftKdQ";
+
+		String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkhIQnlLVS0wRHFBcU1aaDZaRlBkMlZXYU90ZyIsImtpZCI6IkhIQnlLVS0wRHFBcU1aaDZaRlBkMlZXYU90ZyJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuY29yZS53aW5kb3dzLm5ldC8iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8yN2M5NTA0YS1jNDY3LTQzNzYtYjI2MS04OWIxNWU4ZWJiMTcvIiwiaWF0IjoxNTA2NDI3MTA2LCJuYmYiOjE1MDY0MjcxMDYsImV4cCI6MTUwNjQzMTAwNiwiYWNyIjoiMSIsImFpbyI6IlkyVmdZR2crNjVxWi9rUHdmWDNGK1NtTUxoVW1sMlE0UFFOT0hHRStyekU3L0h0OTJIWUEiLCJhbXIiOlsicHdkIl0sImFwcGlkIjoiYjMxNDRmZTEtNjMzOC00M2EwLTg5MTgtYzA1MjkxZjUxMTcwIiwiYXBwaWRhY3IiOiIxIiwiZmFtaWx5X25hbWUiOiJIZXdpdHQiLCJnaXZlbl9uYW1lIjoiSnVzdGluIiwiZ3JvdXBzIjpbImFjNzk2NzQwLWZmNWMtNDdmNy04OWRlLWZlOTMzYmRmNTA4ZCJdLCJpcGFkZHIiOiIxNC4xNDIuMTEwLjIyMiIsIm5hbWUiOiJKdXN0aW4gSGV3aXR0Iiwib2lkIjoiNWY4OTA2ZDEtZWUyZC00MjFkLWFhNzEtMDZhODEwOWMyMTMwIiwicHVpZCI6IjEwMDMzRkZGOEQyNzI0QUMiLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJwcllEaV9sLUdHbE96al9pTDZPa3pwQUt0QW9DZFRlbEcwVnVSczlOUm1ZIiwidGlkIjoiMjdjOTUwNGEtYzQ2Ny00Mzc2LWIyNjEtODliMTVlOGViYjE3IiwidW5pcXVlX25hbWUiOiJqdXN0aW4uaGV3aXR0QHllbGxvd2Zpbi5iaSIsInVwbiI6Imp1c3Rpbi5oZXdpdHRAeWVsbG93ZmluLmJpIiwidXRpIjoiUGRMS2dPQl9iMFdTczE3YzBqWWdBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiNjJlOTAzOTQtNjlmNS00MjM3LTkxOTAtMDEyMTc3MTQ1ZTEwIl19.wJ7k__WncfvlLY5wKG1ej8VDXWbXxmSiAOfld2_ZgIPeQFTMKbalrBZbic9CnTGObHBPBnywQ2sobtsFpcb1xxN11t33KaL_ScETn7Yqwvz6cviHYI0egYuIk9eFyrzgOzRema5jyte4VuJsZSzSWGB6E5Er15a8Gn-PHTo1AJbqKHqa6APruj8qNFOHazqRRkDJ-hYkr6y55X83pwWYy37QzINZjFAVsENpRoImgGjuDIXQmpTPkFiupwA_uuibD3ID6sYEpSCtOJTKiH8B-7sZeIkL1LtQJwUc-rsy3VkokFNx12X0-1xIsPp7cFroA79sBtOCfeQ_14dbqAnx-Q";
 		String CONTENT = "application/json";
 		BigDecimal rate = new BigDecimal(123);
 		JsonArray ja = new JsonArray();
@@ -36,11 +36,12 @@ public class Test2 {
 			JsonElement je = new JsonParser().parse(response.body().string());
 			JsonObject jo = je.getAsJsonObject();
 			JsonArray ja1 = jo.getAsJsonArray("value");
+
+			String resp = Billing.getCard(token);
+			JsonElement match = new JsonParser().parse(resp);
+			JsonArray ja2 = match.getAsJsonArray();
 			for (int j = 0; j < ja1.size(); j++) {
 				JsonObject jo1 = new JsonObject();
-				String resp = Billing.getCard(token);
-				JsonElement match = new JsonParser().parse(resp);
-				JsonArray ja2 = match.getAsJsonArray();
 				String i = ja1.get(j).getAsJsonObject().get("properties").getAsJsonObject().get("meterId")
 						.getAsString();
 				for (int k = 0; k < ja2.size(); k++) {
@@ -49,12 +50,18 @@ public class Test2 {
 						rate = ja2.get(k).getAsJsonObject().get("MeterRates").getAsBigDecimal();
 					}
 				}
-				jo1.addProperty("id", ja1.get(j).getAsJsonObject().get("id").getAsString());
-				jo1.addProperty("SubscriptionId", ja1.get(j).getAsJsonObject().get("properties").getAsJsonObject()
-						.get("subscriptionId").getAsString());
+				// jo1.addProperty("id",
+				// ja1.get(j).getAsJsonObject().get("id").getAsString());
+				jo1.addProperty("Name", ja1.get(j).getAsJsonObject().get("properties").getAsJsonObject()
+						.get("meterName").getAsString());
+				jo1.addProperty("Resource Id", i);
 				BigDecimal quantity = ja1.get(j).getAsJsonObject().get("properties").getAsJsonObject().get("quantity")
 						.getAsBigDecimal();
-				jo1.addProperty("BillAmount(USD)", rate.multiply(quantity));
+				jo1.addProperty("Units", ja1.get(j).getAsJsonObject().get("properties").getAsJsonObject().get("quantity")
+						.getAsBigDecimal());
+				jo1.addProperty("BillAmount(AUD)", rate.multiply(quantity));
+				jo1.addProperty("UsageStartTime", ja1.get(j).getAsJsonObject().get("properties").getAsJsonObject()
+						.get("usageStartTime").getAsString());
 				ja.add(jo1);
 				// System.out.println(ja.toString());
 			}
